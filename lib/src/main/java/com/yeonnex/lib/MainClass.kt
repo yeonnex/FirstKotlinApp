2b212eadd4d6a9ad346446d07a8eccfe4d272894
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-10 01:51:03 +0900

    코틀린 개요

--- lib/src/main/java/com/yeonnex/lib/MainClass.kt
+++ lib/src/main/java/com/yeonnex/lib/MainClass.kt
@@ -3,7 +3,26 @@ package com.yeonnex.lib
 class MainClass {
 
 }
+
+// JVM : 코틀린 소스코드 -> 바이트코드로 변환 -> JVM이 바이트 코드 실행 -> 콘솔출력
+// 데이터타입 : 자바는 (기본 데이터타입 8개 + 참조 데이터타입), 코틀린은 모두 참조 데이터타입
+// 코틀린 내장(built-in)타입 : String, Char, Boolean, Int, Double, 과 컬렉션 타입 List, Set, Map
+// 연산자(우선순위)
+// 메소드(함수)
+// =====================
+// 객체지향(추상화, 캡슐화, 상속, 다형성) -> 리팩토링 + 디자인 패턴
+// 쓰레드, IO, 네트워크
+//  LIBRARY : swing, JavaFX(Win), Spring(Web), Android(mobile)
+
 fun main(args: Array<String>){
+    println("Welcome Kotlin")
+    for (i in 1..8){
+        println("i = $i")
+    }
+    example00()
+}
+
+private fun example00() {
     println(1 + 2)
     println(1 + 2)
     println(1 + 2)
