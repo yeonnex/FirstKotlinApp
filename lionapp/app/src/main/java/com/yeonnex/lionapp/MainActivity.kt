b69430be8e93ec24b3c341115575444bb90f4ec9
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-14 13:28:57 +0900

    라이언과 통화하기

--- lionapp/app/src/main/java/com/yeonnex/lionapp/MainActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.yeonnex.lionapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,10 +21,16 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        })
 
+        // 바로 람다식으로!
         btnSend.setOnClickListener{
             val msg = etMsg.text.toString()
             tvHello.text = msg
             etMsg.setText("")
+        }
+        // 바로 람다식으로!
+        btnCall.setOnClickListener{
+            val intent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222")) // 사진 리소스 등 모든걸 URI라고 함
+            startActivity(intent)
         }
     }
 
