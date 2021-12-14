541cf45b63bc18a58af85e21d1f271589df0826d
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-14 14:39:47 +0900

    카운트 액티비티

--- lionapp/app/src/main/java/com/yeonnex/lionapp/CountActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/CountActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import com.yeonnex.lionapp.databinding.ActivityCountBinding
 
 class CountActivity : AppCompatActivity() {
-
+    private var count: Int = 0
     private lateinit var binding: ActivityCountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCountBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnCount.setOnClickListener {
+            count++
+            binding.tvCnt.text = count.toString()
+        }
     }
 }
