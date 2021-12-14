54b5424d771bf29fd4ff48a2b0b1ffa350a3949d
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-14 13:20:38 +0900

    라이언과 대화하기

--- lionapp/app/src/main/java/com/yeonnex/lionapp/MainActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.yeonnex.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        btnSend.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?){
+//                val msg = etMsg.text.toString()
+//                tvHello.text = msg
+//                etMsg.setText("")
+//            }
+//        })
+
+        btnSend.setOnClickListener{
+            val msg = etMsg.text.toString()
+            tvHello.text = msg
+            etMsg.setText("")
+        }
     }
+
 }
+
