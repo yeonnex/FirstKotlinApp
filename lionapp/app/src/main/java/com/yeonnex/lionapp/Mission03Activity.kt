856e625e962006fd8ca5665af30b55824920201c
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-15 10:18:50 +0900

    안드로이드 레이아웃 배치연습 - Mission03 Galaxy

---
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/Mission03Activity.kt
@@ -0,0 +1,11 @@
+package com.yeonnex.lionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mission03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mission03)
+    }
+}
