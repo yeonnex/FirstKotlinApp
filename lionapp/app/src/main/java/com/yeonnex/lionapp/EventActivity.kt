3073ede59b0b457d869ecb141b89170fce20ea2b
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-15 14:06:23 +0900

    터치와 다중 이벤트 처리하기, Logcat

--- lionapp/app/src/main/java/com/yeonnex/lionapp/EventActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/EventActivity.kt
@@ -2,10 +2,16 @@ package com.yeonnex.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import com.yeonnex.lionapp.databinding.ActivityEventBinding
 
 // 안드로이드 이벤트 처리 개요
+
+
+// 터치와 다중 터치 이벤트 처리하기
+// 터치 이벤트 처리 : OnTouchListener { v: View, m:MotionEvent -> }
 
 class EventActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEventBinding
@@ -29,6 +35,20 @@ class EventActivity : AppCompatActivity() {
             binding.tvStatus.text = "Long Button clicked"
             true // 이벤트를 소비했다는 것을 나타냄
 //            false 이벤트를 완전히 소비하지 않고 같은 타입 리스너에게 계속 전달
+        }
+
+//        binding.activityMain.setOnTouchListener(object: View.OnTouchListener{
+//            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+//                TODO("Not yet implemented")
+//            }
+//        })
+
+        binding.activityMain.setOnTouchListener{v: View, m: MotionEvent ->
+            val pointerCount = m.pointerCount
+            val pointerId = m.getPointerId(0)
+            binding.tvStatus.text = "pointerCount = $pointerCount, pointerId = $pointerId"
+            Log.d("Event...","pointerCount = $pointerCount, pointerId = $pointerId" )
+            true // 이벤트 소비 완료!
         }
 
     }
