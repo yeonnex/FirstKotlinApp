a21b25d225aee3c50a81375d230a7fe5aa5cc5eb
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-15 11:13:14 +0900

    안드로이드 레이아웃 배치연습 - PhotoActivity 이미지 추가

--- lionapp/app/src/main/java/com/yeonnex/lionapp/PhotoActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/PhotoActivity.kt
@@ -2,10 +2,22 @@ package com.yeonnex.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeonnex.lionapp.databinding.ActivityPhotoBinding
 
 class PhotoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPhotoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_photo)
+        binding = ActivityPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUpload.setOnClickListener {
+            binding.imgPhoto.setImageResource(R.drawable.lion)
+        }
+
+        binding.btnDiscard.setOnClickListener {
+            binding.imgPhoto.setImageResource(R.drawable.aff)
+        }
     }
 }
