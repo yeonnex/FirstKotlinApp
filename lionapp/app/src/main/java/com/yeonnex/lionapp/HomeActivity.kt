5d953e8d0abcbc1da610e669108a1528d15f20f9
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-16 13:47:39 +0900

    안드로이드 화면전환 - 네비게이션 홈 화면..코드로 버튼 추가 리팩토링

--- lionapp/app/src/main/java/com/yeonnex/lionapp/HomeActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/HomeActivity.kt
@@ -3,19 +3,33 @@ package com.yeonnex.lionapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import com.oz10.lionapp.MainActivity
 import com.yeonnex.lionapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding:ActivityHomeBinding
+    private val buttons = mutableMapOf<String, Class<*>>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        binding.button11.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-        binding.button12.setOnClickListener {
-            startActivity(Intent(this,CountActivity::class.java))
+
+        buttons["Lion"] = MainActivity::class.java
+        buttons["Count"] = CountActivity::class.java
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+        )
+
+        for (key in buttons.keys){
+            val btn = Button(this)
+            btn.text = key
+            btn.layoutParams =params
+            btn.setOnClickListener {startActivity(Intent(this, buttons[key]))}
+            binding.layoutMenu.addView(btn)
         }
     }
 }
