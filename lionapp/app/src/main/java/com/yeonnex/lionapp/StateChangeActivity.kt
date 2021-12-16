2de0d9988ab7b9848f800be08cfb216125977485
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-16 15:13:26 +0900

    안드로이드 상태를 저장하고 복원

--- lionapp/app/src/main/java/com/yeonnex/lionapp/StateChangeActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/StateChangeActivity.kt
@@ -3,11 +3,12 @@ package com.yeonnex.lionapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.yeonnex.lionapp.databinding.ActivityStateChangeBinding
 
 
 class StateChangeActivity : AppCompatActivity() {
     val TAG = "StateChange"
-
+    private lateinit var binding :ActivityStateChangeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_state_change)
@@ -43,10 +44,15 @@ class StateChangeActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Log.i(TAG, "onSaveInstanceState")
+
+        val userText = binding.editText.text.toString()
+        outState.putCharSequence("savedText", userText)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         Log.i(TAG, "onRestoreInstanceState")
+        val userText = savedInstanceState.getCharSequence("savedText")
+        binding.editText.setText(userText)
     }
 }
