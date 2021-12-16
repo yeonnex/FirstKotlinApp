908c5fb0fe66265e44a21dad97891fd170afbdba
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-16 10:48:08 +0900

    안드로이드 화면 전환 - 값 전달, Bundle

--- lionapp/app/src/main/java/com/yeonnex/lionapp/MemberActivity.kt
+++ lionapp/app/src/main/java/com/yeonnex/lionapp/MemberActivity.kt
@@ -2,10 +2,18 @@ package com.yeonnex.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeonnex.lionapp.databinding.ActivityMemberBinding
 
 class MemberActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMemberBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMemberBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_member)
+
+        val extras = intent.extras?: return
+        val id = extras.getString("id")
+        binding.tvHello.text = id + "님 환영합니다!"
     }
 }
